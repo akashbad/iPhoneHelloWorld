e1d7a11d4e5d8f6360c1cb18ddbbe34aa85b5e45
Akash Badshah <akashbad@mit.edu>
2013-08-15 12:54:59 -0700

    finished hello world iphone app

--- HelloWorld/HelloWorldViewController.m
+++ HelloWorld/HelloWorldViewController.m
@@ -9,6 +9,9 @@
 #import "HelloWorldViewController.h"
 
 @interface HelloWorldViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+- (IBAction)changeGreeting:(id)sender;
 
 @end
 
@@ -26,4 +29,32 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)changeGreeting:(id)sender {
+    self.userName = self.textField.text;
+    
+    NSString *nameString = [self.userName stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    if([nameString length] == 0){
+        nameString = @"World";
+    }
+    
+    NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@!", nameString];
+    
+    self.label.text = greeting;
+}
+
+- (BOOL) textFieldShouldReturn:(UITextField *)theTextField {
+    if(theTextField == self.textField){
+        [theTextField resignFirstResponder];
+    }
+    
+    return YES;
+}
+
+-(void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [[event allTouches] anyObject];
+    if([self.textField isFirstResponder] && [touch view] != self.textField ){
+        [self.textField resignFirstResponder];
+    }
+    [super touchesBegan:touches withEvent:event];
+}
 @end
